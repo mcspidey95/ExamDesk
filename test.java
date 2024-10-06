01f577c7a5cd170f14bba2f8f62d4162432f10a1
mcspidey95 <spideywasthere@gmail.com>
2024-10-06 18:49:38 +0530

    removed garbage

--- test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        int i = 5;
-        int result = (i / 3) + 1;
-        System.out.println("Result: " + result); // Output will be 1
-    }
-}
