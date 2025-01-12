5f1a6ac478180429b37b2346429c4d30ce52c7eb
mcspidey95 <spideywasthere@gmail.com>
2025-01-12 17:57:15 +0530

    added toast

--- src/B_ExamTimetable.svelte
+++ src/B_ExamTimetable.svelte
@@ -17,6 +17,16 @@
   let columnCount = 0;
   let isFileGenerating = false;
   let isSidebarOpen = false;
+  let endTermModeChecked = false;
+  let fixedBreakChecked = false;
+  let slotsPerDay = 0;
+  let examsPerSlot = 0;
+  let studentsPerSlot = 0;
+  let blacklistCourses = "";
+  let saveFlag = false;
+  let toastText = '';
+  let isToastVisible = false;
+  let isUsingOldFiles = false;
 
   loadFileIfExists1('functions/sources/StudentRegistration.txt')
   loadFileIfExists2('functions/sources/FinalCourseData.txt')
@@ -32,7 +42,7 @@
 
     // Restrict to .txt or .tsv files
       if (StudentDataFile1 && (fileExtension !== 'txt')) {
-        alert('Please drop a .txt file');
+        showToast('Please drop a .txt file');
         StudentDataFile1 = null;
       }
 
@@ -49,6 +59,7 @@
       };
 
       reader.readAsText(StudentDataFile1);
+      isUsingOldFiles = false;
 	  }
 	}
 
@@ -63,7 +74,7 @@
 
       // Restrict to .txt or .tsv files
       if (StudentDataFile2 && (fileExtension !== 'txt')) {
-        alert('Please drop a .txt file');
+        showToast('Please drop a .txt file');
         StudentDataFile2 = null;
       }
 
@@ -80,6 +91,7 @@
       };
 
       reader.readAsText(StudentDataFile2);
+      isUsingOldFiles = false;
 	  }
 	}
   
@@ -113,6 +125,7 @@
     };
 
     reader.readAsText(StudentDataFile1);
+    isUsingOldFiles = false;
 	}
 
   function onFileInputChange2(e) {
@@ -133,6 +146,7 @@
     };
 
     reader.readAsText(StudentDataFile2);
+    isUsingOldFiles = false;
 	}
   
 	function removeFile1() {
@@ -166,14 +180,14 @@
 
         if (result.success) {
             console.log(`TSV file saved successfully at ${result.filePath}`);
-            alert(`File saved successfully at: ${result.filePath}`);
+            showToast(fileName + ' saved!')
         } else {
             console.log('Save operation canceled or failed.');
-            alert(result.message || 'File save canceled.');
+            showToast(result.message || 'File save canceled.');
         }
       } catch (error) {
         console.error('Error saving TSV file:', error);
-        alert('An error occurred while saving the file. Check the console for details.');
+        showToast('An error occurred while saving the file. Check the console for details.');
       }
     }
   }
@@ -191,12 +205,14 @@
     const result = await window.api.checkAndLoadFile(fileName);
 
     if (result.exists) {
-        console.log(`File exists. Content loaded.`);
+        console.log(`File 1 exists. Content loaded.`);
+        showToast('Previous files loaded!')
         // Save the file content into a variable
         StudentDataFile1 = {
           name: "StudentRegistration.txt",
           content: result.content,
         };
+        isUsingOldFiles = true;
         console.log(`File saved to variable: ${StudentDataFile1}`);
     } 
   }
@@ -205,33 +221,159 @@
     const result = await window.api.checkAndLoadFile(fileName);
 
     if (result.exists) {
-        console.log(`File exists. Content loaded.`);
+        console.log(`File 2 exists. Content loaded.`);
         // Save the file content into a variable
         StudentDataFile2 = {
           name: "FinalCourseData.txt",
           content: result.content,
         };
+        isUsingOldFiles = true;
         console.log(`File saved to variable: ${StudentDataFile1}`);
     } 
   }
 
   async function generate() {
-    try {
-      console.log('Running Java file...');
-      tsvData = '';
-      isFileGenerating = true;
+    if(StudentDataFile1 && StudentDataFile2){
+      try {
+        console.log('Running Java file...');
+        showToast('Generating Exam Timetable...')
+        tsvData = '';
+        isFileGenerating = true;
       
-      await window.api.runJavaFile('electron/functions/ExamTimetable.class');
+        await window.api.runJavaFile('electron/functions/ExamTimetable.class');
       
-      console.log('Java file execution finished.');
+        console.log('Java file execution finished.');
 
-      setTimeout(() => {
+        setTimeout(() => {
           loadTSVFile('electron/functions/documents/Exam_Timetable.tsv');
-      }, 1000);
-    } catch (error) {
-      console.error('Error during Java file execution:', error);
-      alert('Failed to execute Java file. Check the console for details.');
+        }, 1000);
+
+        showToast('Exam Timetable Generated!')
+      } catch (error) {
+        console.error('Error during Java file execution:', error);
+        showToast('Failed to execute Java file. Check the console for details.');
+      }
     }
+    else showToast('Please Upload The Required Files!')
+  }
+
+  // Function to show the toast
+  function showToast(message) {
+    toastText = message;
+    isToastVisible = true;
+
+    setTimeout(() => {
+      isToastVisible = false;
+    }, 2000);
+  }
+
+  
+  function validateSlotsPerDay(){
+    if(slotsPerDay>3 || slotsPerDay<2){
+      window.api.readTxtFile('electron/functions/metadata/parameters.txt').then((data) => {
+        const match = data.match(/SLOTS_PER_DAY:\s*(\d+)/);
+        if (match) slotsPerDay = parseInt(match[1], 10);
+      });
+
+      showToast("Value must be in the Range [2-3]");
+    }
+  }
+
+  function validateExamsPerSlot(){
+    if(examsPerSlot>2 || examsPerSlot<1){
+      window.api.readTxtFile('electron/functions/metadata/parameters.txt').then((data) => {
+        const match = data.match(/EXAMS_PER_SLOT:\s*(\d+)/);
+        if (match) examsPerSlot = parseInt(match[1], 10);
+      });
+
+      showToast("Value must be in the Range [1-2]");
+    }
+  }
+
+  function validateStudentsPerSlot(){
+    if(studentsPerSlot>8000 || studentsPerSlot<1){
+      window.api.readTxtFile('electron/functions/metadata/parameters.txt').then((data) => {
+        const match = data.match(/STUDENTS_PER_SLOT:\s*(\d+)/);
+        if (match)studentsPerSlot = parseInt(match[1], 10);
+      });
+
+      showToast("Value must be in the Range [1-8000]");
+    }
+  }
+
+  $: {
+    const hoverBg = document.querySelector('.hover-bg');
+    const button = document.querySelector('.button');
+
+    if (hoverBg && button) {
+      if (StudentDataFile1 && StudentDataFile2) {
+        hoverBg.classList.add('translated');
+        button.classList.remove('disabled');
+      } else {
+        hoverBg.classList.remove('translated');
+        button.classList.add('disabled');
+      }
+    }
+  }
+
+  $: if(isSidebarOpen) {
+    saveFlag = true;
+
+    window.api.readTxtFile('electron/functions/metadata/parameters.txt').then((data) => {
+      endTermModeChecked = data.includes("ENDTERM_MODE: true");
+      fixedBreakChecked = data.includes("FIXED_BREAK: true");
+
+      const match = data.match(/SLOTS_PER_DAY:\s*(\d+)/);
+      if (match) {
+        slotsPerDay = parseInt(match[1], 10);
+      }
+
+      const match2 = data.match(/EXAMS_PER_SLOT:\s*(\d+)/);
+      if (match) {
+        examsPerSlot = parseInt(match2[1], 10);
+      }
+
+      const match3 = data.match(/STUDENTS_PER_SLOT:\s*(\d+)/);
+      if (match) {
+        studentsPerSlot = parseInt(match3[1], 10);
+      }
+    }).catch((err) => {
+      console.error(err);
+    });
+
+    window.api.readTxtFile('electron/functions/metadata/blacklist.txt').then((data) => {
+      blacklistCourses = data;
+    }).catch((err) => {
+      console.error(err);
+    });
+  }
+
+  $: if(!isSidebarOpen) {
+      if(saveFlag){
+        let parameterContent = `ENDTERM_MODE: ${endTermModeChecked}
+FIXED_BREAK: ${fixedBreakChecked}
+SLOTS_PER_DAY: ${slotsPerDay}
+EXAMS_PER_SLOT: ${examsPerSlot}
+STUDENTS_PER_SLOT: ${studentsPerSlot}`.trim();
+
+        showToast("Parameters Saved!");
+
+        window.api.writeToTxtFile('electron/functions/metadata/parameters.txt', parameterContent)
+        .then((message) => {
+          console.log(message);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+
+        window.api.writeToTxtFile('electron/functions/metadata/blacklist.txt', blacklistCourses)
+        .then((message) => {
+          console.log(message);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      }
   }
   
 	onMount(() => {
@@ -239,20 +381,17 @@
 	  window.addEventListener('dragleave', onDragLeave);
   
 	  return () => {
-		window.removeEventListener('dragover', onDragOver);
-		window.removeEventListener('dragleave', onDragLeave);
+		  window.removeEventListener('dragover', onDragOver);
+		  window.removeEventListener('dragleave', onDragLeave);
 	  };
 	});
-
-  window.api.readTxtFile('electron/functions/metadata/parameters.txt').then((data) => {
-    console.log(data);
-  }).catch((err) => {
-    console.error(err);
-  });
 </script>
 
 {#if currentView === 'timetable'}
 <body>
+  <div class="toast-container" class:toast-show={isToastVisible}>
+    {toastText}
+  </div>
   <!-- Navbar -->
   <div class="navbar">
     <label for="check" class="sidebar-toggle">
@@ -265,6 +404,13 @@
     <div class="home-button">
       <span class="home-button" on:click={() => currentView = 'home'}><img src="images/home.png" alt="home">Home</span>
     </div>
+
+
+    <div class="indicator">
+      {#if isUsingOldFiles && !isSidebarOpen}
+      <span id="warning">Reusing Previous Files</span>
+      {/if}
+    </div>
     
   </div>
 
@@ -274,7 +420,7 @@
     <div class="parameters">
       <p>EndTerm Mode:</p>
       <label class="switch">
-        <input type="checkbox" id="endTermMode">
+        <input type="checkbox" id="endTermMode" bind:checked={endTermModeChecked}>
         <span class="slider"></span>
       </label>
     </div>
@@ -282,7 +428,7 @@
     <div class="parameters">
       <p>Fixed Break:</p>
       <label class="switch">
-        <input type="checkbox" id="fixedBreak">
+        <input type="checkbox" id="fixedBreak" bind:checked={fixedBreakChecked}>
         <span class="slider"></span>
       </label>
     </div>
@@ -290,29 +436,30 @@
     <div class="parameters">
       <p>Slots Per Day:</p>
       <label class="number">
-        <input type="number" id="slotsPerDay">
+        <input type="number" id="slotsPerDay" bind:value={slotsPerDay} on:blur={() => validateSlotsPerDay()}>
       </label>
     </div>
 
     <div class="parameters">
       <p>Exams Per Slot:</p>
       <label class="number">
-        <input type="number" id="examsPerSlot">
+        <input type="number" id="examsPerSlot" bind:value={examsPerSlot} on:blur={() => validateExamsPerSlot()}>
       </label>
     </div>
 
     <div class="parameters">
       <p>Students Per Slot:</p>
       <label class="number">
-        <input type="number" id="studentsPerSlot">
+        <input type="number" id="studentsPerSlot" bind:value={studentsPerSlot} on:blur={() => validateStudentsPerSlot()}>
       </label>
     </div>
 
+    <p></p>
     <div class="parameters">
       <p>BlackList Courses:</p>
     </div>
 
-    <textarea id="blacklist"></textarea>
+    <textarea id="blacklist scroll_enabled" bind:value={blacklistCourses}></textarea>
   </div>
 
   <!-- Main Content -->
@@ -420,7 +567,7 @@
 
 
     <div class="right-section">
-      <div class="a4-container" on:click={saveTSVFile("ExamTimetable.tsv")}>
+      <div class="a4-container" on:click={() => saveTSVFile("ExamTimetable.tsv")}>
         <!-- Content inside the A4 container -->
         {#if !tsvData}
           {#if isFileGenerating}
@@ -486,6 +633,7 @@ body {
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
   position: relative;  /* Ensures positioning within navbar */
   z-index: 3000;
+  user-select: none;
 }
 
 .home-button {
@@ -500,13 +648,24 @@ body {
 }
 
 .home-button:hover{
-  transform: scale(1.04);
+  transform: translateX(-50%) scale(1.04);
 }
 
 .home-button img {
   width: 30px;
   height: 30px;
   margin-right: 3px;
+}
+
+.indicator {
+  margin-left: auto; /* Pushes the indicator to the right */
+  padding-right: 2rem; /* Optional: Add some spacing from the right edge */
+  padding-bottom: 0.5rem;
+}
+
+#warning{
+  font-size: medium;
+  color: olivedrab;
 }
 
 /* Add a class to scope the styles */
@@ -552,17 +711,20 @@ body {
 .sidebar-toggle input[type="checkbox"]:checked ~ span:nth-of-type(1) {
   transform-origin: bottom;
   transform: rotateZ(45deg) translate(8px, 0px);
+  background: #fff;
 }
 
 .sidebar-toggle input[type="checkbox"]:checked ~ span:nth-of-type(2) {
   transform-origin: top;
   transform: rotateZ(-45deg);
+  background: #fff;
 }
 
 .sidebar-toggle input[type="checkbox"]:checked ~ span:nth-of-type(3) {
   transform-origin: bottom;
   width: 50%;
   transform: translate(30px, -11px) rotateZ(45deg);
+  background: #fff;
 }
 
 /* Main Content Layout */
@@ -571,6 +733,7 @@ body {
   grid-template-columns: 3fr 2fr; /* Left section: 2/3, Right section: 1/3 */
   height: calc(100% - 60px); /* Take the remaining height after navbar */
   width: 100%;
+  user-select: none;
 }
 
 /* Left Section */
@@ -580,10 +743,10 @@ body {
 }
 
 .upload-grid {
-  height: 100%; /* Fill the container's height */
+  height: 100vh; /* Fill the container's height */
   display: grid;
   gap: 1rem; /* Space between upload boxes */
-  grid-template-rows: 2fr 2fr 1fr; /* Top and middle 2/5th, bottom 1/5th */
+  grid-template-rows: 35% 35% 10%; /* Top and middle 2/5th, bottom 1/5th */
   margin-right: 4rem;
   margin-left: 4rem;
 }
